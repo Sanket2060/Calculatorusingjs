582f19c57d2a384fd0a09fd54fbab465bc1eeaa7
Sanket <sanketkarki2060@gmail.com>
2023-09-20 10:20:13 +0545

    html and css done

---
+++ src/calculator.js
@@ -0,0 +1,7 @@
+let operation="";
+
+console.log("Calculator app");
+operation="5+10/10";
+// operation=prompt("Enter the operation","");
+// alert("The operation is:"+operation);
+console.log("The output to the operation is",eval(operation));
